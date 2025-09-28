b3f4f7f8b2f1081558bf7ca8869f4536d6ac267f
Red Tolentino <tolentinored19@gmail.com>
2025-09-28 14:49:38 +0800

    foundation

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,1 +1,61 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/tarantino19/todo"
+)
+
+func main() {
+
+	const todoFileName = ".todo.json"
+
+	// Parsing command line flags
+	task := flag.String("task", "", "Task to be included in the ToDo list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+	flag.Parse()
+
+	l := &todo.List{}
+
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	// Decide what to do based on the provided flags
+	switch {
+	case *list:
+		// List current to do items
+		for _, item := range *l {
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
+		}
+	case *complete > 0:
+		// Complete the given item
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case *task != "":
+		// Add the task
+		l.Add(*task)
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		// Invalid flag provided
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
+	}
+}
--- cmd/todo/main_test.go
+++ cmd/todo/main_test.go
@@ -1,1 +1,67 @@
 package main_test
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"testing"
+)
+
+var (
+	binName  = "todo"
+	fileName = ".todo.json"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("Building tool...")
+	if runtime.GOOS == "windows" {
+		binName += ".exe"
+	}
+	build := exec.Command("go", "build", "-o", binName)
+	if err := build.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot build tool %s: %s", binName, err)
+		os.Exit(1)
+	}
+	fmt.Println("Running tests....")
+	result := m.Run()
+	fmt.Println("Cleaning up...")
+	os.Remove(binName)
+	os.Remove(fileName)
+	os.Exit(result)
+}
+
+func TestTodoCLI(t *testing.T) {
+	task := "test task number 1"
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	cmdPath := filepath.Join(dir, binName)
+
+	t.Run("AddNewTask", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-task", task)
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("ListTasks", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-list")
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Fatal(err)
+		}
+		expected := task + "\n"
+		if expected != string(out) {
+			t.Errorf("Expected %q, got %q instead\n", expected, string(out))
+		}
+	})
+}
+
+// 1. -list: A Boolean flag. When used, the tool will list all to-do items.
+// 2. -task: A string flag. When used, the tool will include the string argument
+// as a new to do item in the list.
+// 3. -complete: An integer flag. When used, the tool will mark the item number
+// as completed.
